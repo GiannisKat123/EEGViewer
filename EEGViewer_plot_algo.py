8c5943b30b95ba38b635fbd03a0b6897a2d9f9e3
GiannisKat123 <97844048+GiannisKat123@users.noreply.github.com>
2023-12-19 15:13:54 +0200

    Update EEGViewer_plot_algo.py

--- EEGViewer_plot_algo.py
+++ EEGViewer_plot_algo.py
@@ -1,6 +1,6 @@
 import sys
 # from PyQt5 import QtWidgets, QtCore
-from qtpy import QtWidgets,QtCore
+from qtpy import QtWidgets,QtCore,QtGui
 from qtpy.QtWidgets import QScrollArea
 from qtpy.QtCore import Qt
 import pyqtgraph as pg
@@ -9,41 +9,55 @@ import pylsl
 from receiver_data import ReceiveData
 from scipy.interpolate import interp1d
 from scipy import signal
+from dataclasses import dataclass
 
-class EEGViewer(QtWidgets.QWidget):
-    def __init__(self,streams:list,color_label:str='k',color_time_marker:str='r',color_per_plot:bool=True,color_for_plots:str='b',background_plot_color:str='k',color_for_markers:str='b',n_seconds_per_screen:int|float = 5):
-        super(EEGViewer, self).__init__()
+@dataclass
+class ViewerConfigs():
+    color_label: str = 'w'
+    color_time_marker: str = 'r'
+    color_per_plot: bool = True
+    color_for_plots: str = 'b'
+    color_for_markers: str = 'b'
+    n_seconds_per_screen: int|float = 5
+    background_plot_color: str = 'k'
+    
 
-        ### Settings for viewer
-        self.color_label = color_label
-        self.color_time_marker = color_time_marker
-        self.color_per_plot = color_per_plot
-        self.color_for_plots = color_for_plots
-        self.background_plot_color = background_plot_color
-        self.n_seconds_per_screen = n_seconds_per_screen
-        self.color_for_markers = color_for_markers
+class Viewer(QtWidgets.QWidget):
+    def __init__(self,streams:list):
+        super(Viewer, self).__init__()
+        
+        self.configs = ViewerConfigs(color_per_plot=False,background_plot_color='w')
         
         self.streams = streams
         
-        self.start_times = []
-        
-        self.start_index = 0
         self.flag_overflow = False
         self.plot_id = 0
-        self.old_plot_id = 0
         self.sampling_freqs = []
         self.num_channels = []
         self.channel_names = []
-        self.downsampled_factors = []
         self.time_marker_ts = []
-        
         
         self.x = []
         self.y = []
-        self.x = []
-        self.current_plot_id = 0
+        self.x_marker_axis = []
+        self.y_marker_axis = []
+        self.timestamps_from_streams = []
         # Set up the layout
-       
+
+        self.streams_data = []
+        self.streams_markers = []
+        
+        self.stream_plots = []
+        self.stream_curves = []
+        self.time_markers1 = []
+        self.markers = []
+        
+        self.samples = []
+        self.start_indexs = []
+        
+        self.initial_plots = []
+        
+        
         ###-----------------------------------------------
         self.lay = pg.GraphicsLayoutWidget()
         self.lay.setFixedHeight(1500)
@@ -51,34 +65,42 @@ class EEGViewer(QtWidgets.QWidget):
         self.setLayout(QtWidgets.QVBoxLayout())
         self.layout().addWidget(self.lay)
         
+        self.overlayed_window = pg.GraphicsLayoutWidget(self.lay)
+        self.overlayed_window.setFixedHeight(1500)
+        self.overlayed_window.setFixedWidth(1700)
+        self.overlayed_window.setBackground(None)
+        self.overlayed_window.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
+        
+        label = pg.LabelItem("PSEUDOLABEL",color=self.configs.background_plot_color)
+        # label.hideAxis('bottom')
+        self.overlayed_window.addItem(label, row=0,col=0)
+        # self.overlayed_window.hideAxis('bottom')
+        
         ###-----------------------------------------------
         
         # Create plot widgets for each channel
-        self.streams_data = []
-        self.streams_markers = []
-        
-        self.stream_plots = []
-        self.stream_curves = []
-        self.time_markers = []
-        self.markers = []
-        
-        self.samples = []
-        self.start_indexs = []
-        
+
         for inlet,stream_id in self.streams:
             if inlet.inlet_type == "Markers":
                 self.streams_markers.append(inlet)
-        
             else:
                 self.streams_data.append(inlet)
         
-        for stream in self.streams_data:
-            self.start_times.append(stream.time_created)
+        self.plotWidget1 = self.overlayed_window.addPlot(row=0, col=1)
+        self.plotWidget1.hideAxis('left')
+        self.plotWidget1.setYRange(0, 0.99, padding=0)
+        self.plotWidget1.hideAxis('bottom')
+
+        
+        for marker_stream in range(len(self.streams_markers)):
+            marker1 = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen(self.configs.color_for_markers, width=5))
+            self.plotWidget1.addItem(marker1)
+            self.markers.append(marker1)
+        
         
         for stream in self.streams_data:
-            self.old_plot_id = self.plot_id
+            
             curves = []
-            time_markers = []
             self.samples.append(0)
             self.start_indexs.append(0)
             
@@ -89,51 +111,51 @@ class EEGViewer(QtWidgets.QWidget):
             self.sampling_freqs.append(sampling_freq)
             self.num_channels.append(num_channels)
             
+            self.x_marker_axis.append(np.linspace(0,self.configs.n_seconds_per_screen,int(sampling_freq * self.configs.n_seconds_per_screen)))
+            self.y_marker_axis.append(np.ones((int(sampling_freq * self.configs.n_seconds_per_screen),)))
+            
+            time_marker1 = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen(self.configs.color_time_marker, width=5))
+            self.plotWidget1.addItem(time_marker1)
+            self.time_markers1.append(time_marker1)
+            
+            curve = self.plotWidget1.plot(pen='k')
+            curve.setData(self.x_marker_axis[-1],self.y_marker_axis[-1])
+            
             for chan_id in channel_names:
                 self.channel_names.append(chan_id)
             
             print(num_channels,channel_names,sampling_freq)
             
             for chan_id in range(num_channels):
-                label = pg.LabelItem(channel_names[chan_id],color=self.color_label)
+                label = pg.LabelItem(channel_names[chan_id],color=self.configs.color_label)
                 self.lay.addItem(label, row=self.plot_id,col=0)
-                self.lay.setBackground(self.background_plot_color)
+                self.lay.setBackground(self.configs.background_plot_color)
                 
                 plotWidget = self.lay.addPlot(row=self.plot_id, col=1, sharex=self.stream_plots[0] if self.plot_id > 0 else None)
-
-                time_marker = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen(self.color_time_marker, width=5))
-                plotWidget.addItem(time_marker)
-                time_markers.append(time_marker)
                 
                 plotWidget.enableAutoRange('y', True)  # Enable autorange for y-axis
                 plotWidget.hideAxis('left')  # Hide y-axis for clarity
                 
-                for marker_stream in self.streams_markers:
-                    marker = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen(self.color_for_markers, width=5))
-                    plotWidget.addItem(marker)
-                    self.markers.append(marker)
+                # if chan_id!=num_channels-1:
+                    # plotWidget.hideAxis('bottom')
                 
-                if chan_id!=num_channels-1:
-                    plotWidget.hideAxis('bottom')
-                
-                if self.color_per_plot:
+                if self.configs.color_per_plot:
                     curve = plotWidget.plot(pen=(self.plot_id, num_channels))
                 else:
-                    curve = plotWidget.plot(pen=self.color_for_plots)
+                    curve = plotWidget.plot(pen=self.configs.color_for_plots)
 
                 self.stream_plots.append(plotWidget)
                 curves.append(curve)
                 
                 self.plot_id += 1
-        
-            # self.time_markers.append(time_marker)
             self.stream_curves.append(curves)  
-            self.time_markers.append(time_markers)  
+        
+        self.initial_plots = self.stream_plots
         
         # # Initialize data arrays
         for stream_id in range(len(self.streams_data)):
-            self.y.append(np.zeros((int(self.sampling_freqs[stream_id] * self.n_seconds_per_screen),self.num_channels[stream_id])))
-            self.x.append(np.linspace(0, self.n_seconds_per_screen, int(self.sampling_freqs[stream_id] * self.n_seconds_per_screen)))
+            self.y.append(np.zeros((int(self.sampling_freqs[stream_id] * self.configs.n_seconds_per_screen),self.num_channels[stream_id])))
+            self.x.append(np.linspace(0, self.configs.n_seconds_per_screen, int(self.sampling_freqs[stream_id] * self.configs.n_seconds_per_screen)))
 
         # Set up a timer for updates
         self.timer = pg.QtCore.QTimer()
@@ -144,41 +166,42 @@ class EEGViewer(QtWidgets.QWidget):
     def toggle_channel_visibility(self, channel_index, is_visible):
         """Toggle the visibility of the specified channel."""
         self.stream_plots[channel_index].setVisible(is_visible)
-    
-    def find_closest_id(self,x_axis,ts):
-        print(len(x_axis))
+        
+        # del self.stream_curves[channel_index]
+        
+        
+    def find_index_in_stream(self,x_axis,ts):
         dif = 10e5
         for index in range(len(x_axis)):
-            if abs(x_axis[index] - ts) < dif:
+            # print(abs(x_axis[index] - ts),dif)
+            if abs(x_axis[index] - ts) < dif and index==len(x_axis)-1:
+                print(abs(x_axis[index] - ts))
+                return index
+            
+            elif abs(x_axis[index] - ts) < dif:
                 dif = abs(x_axis[index] - ts)
-            else:
-                print(x_axis[index])
-                return x_axis[index] 
+            
+            elif abs(x_axis[index] - ts) > dif:
+                print(abs(x_axis[index] - ts))
+                return index
                 
     def update(self):
-        self.current_plot_id = 0
+        self.timestamps_from_streams = []
+        
+        marker_samples = self.samples
         
         for stream_id in range(len(self.streams_data)):
         
             new_data,time_ = self.streams_data[stream_id].update()
             
             if time_:
+                self.timestamps_from_streams.append(time_)
                 
                 ts_after_overflow = 0        
                 
-                # if self.downsampled_factors[stream_id]!=1:
-                #     transposed_data = np.transpose(new_data)
-                    
-                #     new_data = np.transpose(signal.decimate(transposed_data,self.downsampled_factors[stream_id]))
-                    
-                #     max_time = np.max(time_)
-                #     min_time = np.min(time_)
-                    
-                #     time_ = np.linspace(min_time,max_time,new_data.shape[0])
-                
-                if self.samples[stream_id] + new_data.shape[0] >= self.sampling_freqs[stream_id] * self.n_seconds_per_screen:
+                if self.samples[stream_id] + new_data.shape[0] >= self.sampling_freqs[stream_id] * self.configs.n_seconds_per_screen:
                     self.flag_overflow = True
-                    ts_after_overflow = int(self.sampling_freqs[stream_id] * self.n_seconds_per_screen - self.samples[stream_id])
+                    ts_after_overflow = int(self.sampling_freqs[stream_id] * self.configs.n_seconds_per_screen - self.samples[stream_id])
             
                 if self.flag_overflow == True:
                     if ts_after_overflow > 0:
@@ -194,44 +217,56 @@ class EEGViewer(QtWidgets.QWidget):
                         self.y[stream_id][self.start_indexs[stream_id]:self.samples[stream_id]] = new_data[ts_after_overflow:]
                         self.flag_overflow = False
                         self.start_indexs[stream_id] += self.samples[stream_id]
-                        self.current_plot_id += self.num_channels[stream_id]
                                     
                     else:
                         self.start_indexs[stream_id] = 0
                         self.samples[stream_id] = new_data.shape[0]
                         ts = self.x[stream_id][self.samples[stream_id]]
                         
+                        self.time_markers1[stream_id].setValue(ts)  
+                        
                         self.y[self.start_indexs[stream_id]:self.samples[stream_id]] = new_data
                                                 
                         for i in range(self.num_channels[stream_id]):
                             self.stream_curves[stream_id][i].setData(self.x[stream_id], self.y[stream_id][:,i])   
-                            self.time_markers[stream_id][i].setValue(ts)
-                            self.time_markers[stream_id].setValue(ts)
-                            
+                        
                         self.start_indexs[stream_id] += self.samples[stream_id]
                         self.flag_overflow = False
-                        self.current_plot_id += self.num_channels[stream_id]
 
                 else:
                     self.samples[stream_id] += new_data.shape[0]
                     ts = self.x[stream_id][self.samples[stream_id]]
                     
+                    self.time_markers1[stream_id].setValue(ts)
+                    
                     self.y[stream_id][self.start_indexs[stream_id]:self.samples[stream_id]] = new_data
                     for i in range(self.num_channels[stream_id]):
-                        self.time_markers[stream_id][i].setValue(ts)
                         self.stream_curves[stream_id][i].setData(self.x[stream_id], self.y[stream_id][:,i])   
-                        
+                    
                     self.start_indexs[stream_id] = self.samples[stream_id]
-                    self.current_plot_id += self.num_channels[stream_id]
               
         self.marker_current_id = 0     
         for stream_id in range(len(self.streams_markers)):    
             new_data,time_ = self.streams_markers[stream_id].update()
             if time_:
-                self.time_marker_ts.append([])
-                for i in range(len(self.streams_data)):
-                    marker_ts = self.time_markers[i][0].getXPos()
-                    for mark_id in range(self.marker_current_id,self.marker_current_id+self.num_channels[i]):
-                        self.markers[mark_id].setValue(marker_ts)
-                    self.marker_current_id += self.num_channels[i]
+                if not self.timestamps_from_streams:
+                    self.time_marker_ts.append(time_[0])
+                else:
+                    if not self.time_marker_ts:
+                        print("NO")
+                        for i in range(len(self.timestamps_from_streams)):
+                            marker_ts_index = self.find_index_in_stream(x_axis=self.timestamps_from_streams[i],ts=time_[0])
+                            print(marker_ts_index,len(self.timestamps_from_streams[i]))
+                            print(marker_ts_index,self.x_marker_axis[i][marker_samples[i] + marker_ts_index])
+                            self.markers[stream_id].setValue(self.x_marker_axis[i][marker_samples[i] + marker_ts_index])
+                    else:
+                        print("YES")
+                        self.time_marker_ts.append(time_[0])
+                        for i in range(len(self.timestamps_from_streams)):
+                            marker_ts_index = self.find_index_in_stream(x_axis=self.timestamps_from_streams[i],ts=self.time_marker_ts[0])
+                            print(marker_ts_index,self.x_marker_axis[i][marker_samples[i] + marker_ts_index])
+                            self.markers[stream_id].setValue(self.x_marker_axis[i][marker_samples[i] + marker_ts_index])
+                            del self.time_marker_ts[0]
+
+        self.timestamps_from_streams = []   
             
